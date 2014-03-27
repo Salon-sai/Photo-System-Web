02ed4e324e6938b3e6733d5589fbca1187cee11f
Salon-sai <coke1231078@gmail.com>
2014-03-27 11:09:01 +0800

    find user following post

--- src/main/java/saiwei/dao/impl/PostDAOImpl.java
+++ src/main/java/saiwei/dao/impl/PostDAOImpl.java
@@ -3,10 +3,17 @@
  */
 package saiwei.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import saiwei.dao.IPostDAO;
 import saiwei.model.Post;
+import saiwei.model.User;
 
 import common.BaseDAO;
 
@@ -17,4 +24,25 @@ import common.BaseDAO;
 @Repository("postDAO")
 public class PostDAOImpl extends BaseDAO<Post> implements IPostDAO {
 
+	public List<Post> findPostByUserFollowingINCriteria(String userIdNum){
+		Session session = sessionFactory.getCurrentSession();
+		List<Post> showpost = new ArrayList<Post>();
+		List<User> followings = UserDAOImpl.findfollowingINCriteria(session, userIdNum);
+		
+		for(User poster : followings){
+			Criteria criteria = session.createCriteria(Post.class);
+			criteria.add(Restrictions.eq("poster", poster));
+			@SuppressWarnings("unchecked")
+			List<Post> posts = criteria.setMaxResults(3).list();
+			showpost.addAll(posts);
+		}
+		
+		return null;
+	}
+	
+	public List<Post> findPostByUserFollowingINSQL(String userIdNum){
+		
+		return null;
+	}
+	
 }

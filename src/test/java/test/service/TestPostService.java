0a3daeb591c8a4ee57cd2fe3764f417dd9e76660
Salon.sai <coke1231078@gmail.com>
2014-04-13 15:31:16 +0800

    test post service

---
+++ src/test/java/test/service/TestPostService.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package test.service;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import saiwei.model.Post;
+import saiwei.model.User;
+import saiwei.service.IPostService;
+import saiwei.service.IUserService;
+import test.common.CommonTester;
+
+/**
+ * @author sai
+ *
+ */
+public class TestPostService extends CommonTester {
+
+	@Test
+	public void testSave(){
+		IUserService userservice = (IUserService)context.getBean("userService");
+		IPostService postservice = (IPostService)context.getBean("postService");
+		
+		User poster = userservice.login("a", "a");
+		
+		Post post = new Post();
+		post.setPoster(poster);
+		post.setContent("aaaaaaa");
+		post.setPostdate(new Date());
+		postservice.save(post);
+		
+		post.setContent("bbbbb");
+		post.setPostdate(new Date());
+		postservice.save(post);
+		
+		post.setContent("ccccc");
+		post.setPostdate(new Date());
+		postservice.save(post);
+		
+		
+	}
+	
+}

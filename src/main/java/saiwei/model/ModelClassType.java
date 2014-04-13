38b86bda0bf4c66afc74bbad7b1f3688751aa118
Salon.sai <coke1231078@gmail.com>
2014-04-13 16:26:32 +0800

    model type class

---
+++ src/main/java/saiwei/model/ModelClassType.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package saiwei.model;
+
+/**
+ * @author sai
+ *
+ */
+public abstract class ModelClassType {
+	
+	protected static final String COMMENT = "Comment";
+	
+	protected static final String MANAGER = "Manger";
+	
+	protected static final String PHOTO = "Photo";
+	
+	protected static final String POST = "Post";
+	
+	protected static final String PROFILE = "Profile";
+	
+	protected static final String RELATIONSHIP = "RelationShip";
+	
+	protected static final String RELATIONSHIPTYPE = "RelationShipType" ;
+	
+	protected static final String TAG = "Tag";
+	
+	protected static final String TITLE = "Title";
+	
+	protected static final String USER = "User";
+	
+	protected static final String USERTYPE = "UserType";
+}

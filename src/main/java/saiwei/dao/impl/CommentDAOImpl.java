878cadaf0aac64898a76388915de794ea6aba2a7
Salon.sai <coke1231078@gmail.com>
2014-04-13 16:29:34 +0800

    DAO commit the comment to database

--- src/main/java/saiwei/dao/impl/CommentDAOImpl.java
+++ src/main/java/saiwei/dao/impl/CommentDAOImpl.java
@@ -3,10 +3,16 @@
  */
 package saiwei.dao.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import saiwei.dao.ICommentDAO;
 import saiwei.model.Comment;
+import saiwei.model.Post;
+import saiwei.model.User;
 
 import common.BaseDAO;
 
@@ -17,4 +23,38 @@ import common.BaseDAO;
 @Repository("commentDAO")
 public class CommentDAOImpl extends BaseDAO<Comment> implements ICommentDAO {
 
+	/**
+	 * 
+	 * @param transientInstance
+	 * 		comment instance creating in service
+	 * @param posterId
+	 * 		user posting comment
+	 * @param recipientId
+	 * 		recipient recipientting comment
+	 */
+	public void save(Comment transientInstance,String posterId,String recipientId,String postId) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("IdNumber", posterId);
+		User poster = (User)this.uniqueElement(params, USER);
+		
+		params.clear();
+		
+		params.put("IdNumber", recipientId);
+		User recipients = (User)this.uniqueElement(params, USER);
+		
+		params.clear();
+		
+		params.put("id", postId);
+		Post post = (Post)this.uniqueElement(params, POST);
+		
+		transientInstance.setPost(post);
+		transientInstance.setPoster(poster);
+		transientInstance.setRecipients(recipients);
+		
+		session.save(transientInstance);
+	}
+	
 }

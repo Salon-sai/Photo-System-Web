198d552799b30ea69074e8bb54778758e4aeed20
Salon-sai <coke1231078@gmail.com>
2014-04-14 08:16:53 +0800

    update

--- src/test/java/test/common/CommonTester.java
+++ src/test/java/test/common/CommonTester.java
@@ -4,18 +4,18 @@ import javax.persistence.MappedSuperclass;
 
 import org.apache.log4j.Logger;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @MappedSuperclass
 public class CommonTester {
 	
-	protected ApplicationContext context;
+	protected static ApplicationContext context;
 	protected static final Logger logger = Logger.getLogger(CommonTester.class);
 	
-	@Before
-	public void init(){
+	@BeforeClass
+	public static void init(){
 		context = new ClassPathXmlApplicationContext(new String[]{"spring.xml","spring-hibernate.xml"});
 		logger.info("before to test in Common Tester");
 	}

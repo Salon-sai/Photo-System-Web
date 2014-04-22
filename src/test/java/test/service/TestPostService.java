d551f5643f5254404f335c489247ed113e06a16c
Salon-sai <coke1231078@gmail.com>
2014-04-22 22:41:56 +0800

    update test service

--- src/test/java/test/service/TestPostService.java
+++ src/test/java/test/service/TestPostService.java
@@ -5,6 +5,8 @@ package test.service;
 
 import java.util.Date;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
 import saiwei.model.Post;
@@ -19,7 +21,7 @@ import test.common.CommonTester;
  */
 public class TestPostService extends CommonTester {
 
-	@Test
+//	@Test
 	public void testSave(){
 		IUserService userservice = (IUserService)context.getBean("userService");
 		IPostService postservice = (IPostService)context.getBean("postService");
@@ -43,4 +45,21 @@ public class TestPostService extends CommonTester {
 		
 	}
 	
+//	@Test
+	public void testsaveLikePost(){
+		IPostService postService = (IPostService)context.getBean("postService");
+		
+		boolean flag = postService.saveLikePost("b", "8a8a1f79456a99d701456a99df470000");
+		
+		TestCase.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testdeleteLikePost(){
+		IPostService postService = (IPostService)context.getBean("postService");
+		
+		boolean flag = postService.deleteLikePost("b", "8a8a1f79456a99d701456a99df470000");
+		
+		TestCase.assertEquals(true, flag);
+	}
 }

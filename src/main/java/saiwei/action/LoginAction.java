299a99630b47bf3221cbca58aa87e574125fb2e6
Salon-sai <coke1231078@gmail.com>
2014-05-08 13:10:30 +0800

    check login

--- src/main/java/saiwei/action/LoginAction.java
+++ src/main/java/saiwei/action/LoginAction.java
@@ -1,5 +1,37 @@
 package saiwei.action;
 
-public class LoginAction {
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
 
+import com.opensymphony.xwork2.ActionSupport;
+
+import saiwei.model.User;
+import saiwei.service.IUserService;
+
+@Action(value="loginAction")
+@Namespace("/")
+public class LoginAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private IUserService userService;
+	private String idNum;
+	private String password;
+	
+	public IUserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(IUserService userService) {
+		this.userService = userService;
+	}
+	
+	public String checkLogin(){
+		User user = userService.login(idNum, password);
+		if(user == null)
+			return INPUT;
+		return SUCCESS;
+	}
 }

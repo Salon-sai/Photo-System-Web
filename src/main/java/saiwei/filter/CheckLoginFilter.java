e2cfe1d07ed622876b84a69056ddfb615de40516
Salon-sai <coke1231078@gmail.com>
2014-05-11 14:09:14 +0800

    checkLogin filter

---
+++ src/main/java/saiwei/filter/CheckLoginFilter.java
@@ -0,0 +1,46 @@
+package saiwei.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 
+ * @author sai
+ *
+ */
+public class CheckLoginFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		if(null != session.getAttribute("user")){
+			chain.doFilter(request, response);
+		}else{
+			req.getRequestDispatcher("login.jsp").forward(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

ca33f799deb805bd480684930b151291ca74a461
Salon-sai <coke1231078@gmail.com>
2014-05-24 00:54:40 +0800

    create comment bean

--- src/main/java/saiwei/bean/CommentBean.java
+++ src/main/java/saiwei/bean/CommentBean.java
@@ -1,6 +1,7 @@
 package saiwei.bean;
 
 import saiwei.model.Comment;
+import saiwei.model.User;
 
 public class CommentBean extends Comment{
 
@@ -10,10 +11,30 @@ public class CommentBean extends Comment{
 	private static final long serialVersionUID = 1L;
 	
 	private String posterName;
+	private String posterNumber;
 	private String recipientName;
+	private String recipientNumber;
 	private String postId;
 	
-
+	public CommentBean(Comment comment){
+		this.id = comment.getId();
+		this.content = comment.getContent();
+		this.postdate = comment.getPostdate();
+		
+		User poster = comment.getPoster();
+		User recipient = comment.getRecipients();
+		
+		this.posterNumber = poster.getIdNumber();
+		this.posterName = poster.getName();
+		
+		this.recipientNumber = recipient.getIdNumber();
+		this.recipientName = recipient.getName();
+	}
+	
+	public CommentBean(){
+		
+	}
+	
 	public String getPosterName() {
 		return posterName;
 	}
@@ -31,5 +52,17 @@ public class CommentBean extends Comment{
 	}
 	public void setPostId(String postId) {
 		this.postId = postId;
+	}
+	public String getPosterNumber() {
+		return posterNumber;
+	}
+	public void setPosterNumber(String posterNumber) {
+		this.posterNumber = posterNumber;
+	}
+	public String getRecipientNumber() {
+		return recipientNumber;
+	}
+	public void setRecipientNumber(String recipientNumber) {
+		this.recipientNumber = recipientNumber;
 	}
 }
